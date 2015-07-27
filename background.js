2c3dc10066ca9e2e62d2c234394aab1f3e0cc368
Jonathan Sharifi <jonathansharifi@gmail.com>
2015-07-27 21:48:07 +0100

    Jon: Updating manifest and adding background.js

---
+++ background.js
@@ -0,0 +1,49 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ url: "http://project-jordan.herokuapp.com/jordan" });
+});
+
+setInterval(function()
+{
+$.getJSON("http://project-jordan.herokuapp.com/jordan/api/all?format=json", function(result){
+      var currentItems = new Array();
+      var items = localStorage.getItem("items");
+      if(items === null) {
+        items = new Array();
+      }
+
+      $.each(items, function(i, field){
+        var obj = jQuery.parseJSON( field );
+        currentItems.push(obj.name);
+      });
+
+      var newItems = new Array();
+      $.each(result, function(i, field){
+        var obj = jQuery.parseJSON( field );
+        newItems.push(obj.name);
+      });
+
+      if(currentItems.length === 0){
+        localStorage.setItem("items", result);
+      }
+
+      if(newItems.length > currentItems.length) {
+        var newlyListedItems;
+        newlyListedItems = $(newItems).not(currentItems).get();
+        var count = 0;
+        $.each(newlyListedItems, function(i, field){
+          var opt = {
+            type: "basic",
+            title: field,
+            message: "is now on Project Jordan",
+            iconUrl: "icon.png"
+          }
+
+          chrome.notifications.create("Added"+count, opt, function(data) {
+            console.log(data);
+          });
+          count++;
+        });
+        localStorage.setItem("items", result);
+      }
+});
+}, 60000);
